50c457dae6fb06eabb71b22f392c70186f25909f
bguillaum <bguillaum@7838e531-6607-4d57-9587-6c381814729c>
2014-02-20 22:03:57 +0000

    new command lien argument new infos field (replace the old features)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/semagramme/dep2pict/trunk@8173 7838e531-6607-4d57-9587-6c381814729c

--- src/gui.ml
+++ src/gui.ml
@@ -5,22 +5,38 @@ open Ui
 open Rsvg
 open GMain
 
-let (input_file : string option ref) = ref None
-let (output_file : string option ref) = ref None
+(* -------------------------------------------------------------------------------- *)
+let writeFile path str =
+  let file = open_out path in
+  output file str 0 (String.length str);
+  flush file;
+  close_out file
+ 	
+(* -------------------------------------------------------------------------------- *)
+let readFile path =
+  let text =
+    let file = open_in path in
+    let fileSize = in_channel_length file in
+    let buffer = String.create fileSize in
+    really_input file buffer 0 fileSize;
+    close_in file;
+    buffer in text
 
+
+(* -------------------------------------------------------------------------------- *)
+let (current_file : string option ref) = ref None
+let current_infos = ref ["pos"]
+let current_source = ref ""
+let current_zoom = ref 100.0
+
+(* -------------------------------------------------------------------------------- *)
 let refresh_svg (ui:ui) () =
-  let input_text =
-    ui#dep_source#buffer#get_text 
-      ~start:ui#dep_source#buffer#start_iter 
-      ~stop:ui#dep_source#buffer#end_iter 
-      ~slice:true 
-      ~visible:true () in
   try
     let svg =
-      if input_text <> "" && input_text.[0] = '1'
-      then Dep2pict.fromConllStringToSvgString input_text
-      else Dep2pict.fromDepStringToSvgString input_text in
-    let pixbuf = Rsvg.render_from_string ~size_cb:(Rsvg.at_zoom 1.0 1.0) svg in
+      if !current_source <> "" && !current_source.[0] = '1'
+      then Dep2pict.fromConllStringToSvgString ~infos:(!current_infos) !current_source
+      else Dep2pict.fromDepStringToSvgString !current_source in
+    let pixbuf = Rsvg.render_from_string ~size_cb:(Rsvg.at_zoom (!current_zoom /. 100.) (!current_zoom /. 100.)) svg in
     ui#svg_view#set_pixbuf pixbuf;
     ui#error_view#buffer#set_text "";
   with
@@ -32,121 +48,133 @@ let refresh_svg (ui:ui) () =
     | Dep2pict.Loop_in_dep msg -> ui#error_view#buffer#set_text ("Loop in dependency : "^msg)
     | Dep2pict.Conll_format msg -> ui#error_view#buffer#set_text ("Conll format : "^msg)
 
-let writeFile path str =
-  let file = open_out path in
-  output file str 0 (String.length str);
-  flush file;
-  close_out file
- 	
-let readFile path =
-  let text =
-    let file = open_in path in
-    let fileSize = in_channel_length file in
-    let buffer = String.create fileSize in
-    really_input file buffer 0 fileSize;
-    close_in file;
-    buffer in text
 
-let save (ui:ui) () =
-	let compute file () =
-		writeFile file#filename (ui#dep_source#buffer#get_text 
-			~start:ui#dep_source#buffer#start_iter 
-			~stop:ui#dep_source#buffer#end_iter 
-			~slice:true 
-			~visible:true ())
-	in
-	let sel = GWindow.file_selection ~title:"Save file" ~show:true() in
-	ignore(sel#ok_button#connect#clicked ~callback:(compute sel));
-	ignore(sel#ok_button#connect#clicked ~callback:(sel#destroy));
-	ignore(sel#cancel_button#connect#clicked ~callback:(sel#destroy));
-	sel#complete ~filter:"*.dep";
-	()
+(* -------------------------------------------------------------------------------- *)
+let save_as (ui:ui) () =
+  let sel = GWindow.file_selection ~title:"Save file" ~show:true() in
+  (* TODO deal with dep|conll... sel#complete ~filter:"*.conll"; *)
 
-let open_dep (ui:ui) () =
-  let compute file () =
-    ui#dep_source#buffer#set_text (readFile file#filename);
-    input_file := Some file#filename in
-  let sel = GWindow.file_selection ~title:"Open file" ~show:true() in
-  ignore(sel#ok_button#connect#clicked ~callback:(compute sel));
-  ignore(sel#ok_button#connect#clicked ~callback:(sel#destroy));
-  ignore(sel#cancel_button#connect#clicked ~callback:(sel#destroy));
-  sel#complete ~filter:"*.dep";
+  let _ = sel#ok_button#connect#clicked
+    ~callback: (fun () ->
+      writeFile sel#filename !current_source;
+      current_file := Some sel#filename;
+      sel#destroy ()
+    ) in
+
+  let _ = sel#cancel_button#connect#clicked ~callback:(sel#destroy) in
   ()
 
+(* -------------------------------------------------------------------------------- *)
+let save (ui:ui) () =
+  match !current_file with
+    | Some file -> writeFile file !current_source
+    | None -> save_as (ui:ui) ()
 
+(* -------------------------------------------------------------------------------- *)
+let open_dep (ui:ui) () =
+  let sel = GWindow.file_selection ~title:"Open file" ~show:true() in
+  (* TODO deal with dep|conll... sel#complete ~filter:"*.conll"; *)
+
+  let _ = sel#ok_button#connect#clicked
+    ~callback: (fun () -> 
+      current_source := readFile sel#filename;
+      current_file := Some sel#filename;
+      sel#destroy ()
+    ) in
+
+  let _ = sel#cancel_button#connect#clicked ~callback:(sel#destroy) in
+  ()
+
+(* -------------------------------------------------------------------------------- *)
 let reload (ui:ui) () =
-  match !input_file with
-    | Some filename -> ui#dep_source#buffer#set_text (readFile filename)
+  match !current_file with
+    | Some filename -> current_source := readFile filename
     | None -> ()
 
-let to_svg (ui:ui) () =
-	let compute file () =
-		writeFile file#filename (Dep2pict.fromDepStringToSvgString (ui#dep_source#buffer#get_text 
-			~start:ui#dep_source#buffer#start_iter 
-			~stop:ui#dep_source#buffer#end_iter 
-			~slice:true 
-			~visible:true ()))
-	in
-	let sel = GWindow.file_selection ~title:"Save file as svg" ~show:true() in
-	ignore(sel#ok_button#connect#clicked ~callback:(compute sel));
-	ignore(sel#ok_button#connect#clicked ~callback:(sel#destroy));
-	ignore(sel#cancel_button#connect#clicked ~callback:(sel#destroy));
-	sel#complete ~filter:"*.svg";
-	()
-	
-let to_png (ui:ui) () =
-	let compute file () =
-		ignore(Dep2pict.fromDepStringToPng (ui#dep_source#buffer#get_text 
-			~start:ui#dep_source#buffer#start_iter 
-			~stop:ui#dep_source#buffer#end_iter 
-			~slice:true 
-			~visible:true ()) file#filename)
-	in
-	let sel = GWindow.file_selection ~title:"Save file as png" ~show:true() in
-	ignore(sel#ok_button#connect#clicked ~callback:(compute sel));
-	ignore(sel#ok_button#connect#clicked ~callback:(sel#destroy));
-	ignore(sel#cancel_button#connect#clicked ~callback:(sel#destroy));
-	sel#complete ~filter:"*.png";
-	()
-	
-let to_pdf (ui:ui) () =
-	let compute file () =
-		ignore(Dep2pict.fromDepStringToPdf (ui#dep_source#buffer#get_text 
-			~start:ui#dep_source#buffer#start_iter 
-			~stop:ui#dep_source#buffer#end_iter 
-			~slice:true 
-			~visible:true ()) file#filename)
-	in
-	let sel = GWindow.file_selection ~title:"Save file as pdf" ~show:true() in
-	ignore(sel#ok_button#connect#clicked ~callback:(compute sel));
-	ignore(sel#ok_button#connect#clicked ~callback:(sel#destroy));
-	ignore(sel#cancel_button#connect#clicked ~callback:(sel#destroy));
-	sel#complete ~filter:"*.pdf";
-	()
+(* -------------------------------------------------------------------------------- *)
+let export format (ui:ui) () =
+  let title = sprintf "Save file as %s" format in
+  let file_window = GWindow.file_selection ~title ~show:true() in
+  let _ = file_window#ok_button#connect#clicked
+    ~callback:(fun () -> 
+      begin
+        let dep_code =
+          if String.length !current_source > 0 && !current_source.[0] = '1'
+          then Dep2pict.fromConllStringToDepString !current_source
+          else !current_source in
+        match format with
+          | "svg" -> writeFile file_window#filename (Dep2pict.fromDepStringToSvgString dep_code)
+          | "png" -> ignore (Dep2pict.fromDepStringToPng dep_code file_window#filename)
+          | "pdf" -> ignore (Dep2pict.fromDepStringToPdf dep_code file_window#filename)
+          | _ -> failwith (sprintf "Unknown format %s" format)
+      end;
+      file_window#destroy ()
+    ) in
+  let _ =  file_window#cancel_button#connect#clicked ~callback:(file_window#destroy) in
+  let _ = file_window#complete ~filter:("*."^format) in
+  ()
 
-let main ?file () = 
-  ignore (GMain.Main.init ());
-  let ui = new ui () in 
-  ui#toplevel#set_title ("Dep2pict v. "^VERSION);
-	
-	
-  ignore(ui#toplevel#connect#destroy ~callback:GMain.Main.quit);
-  ignore(ui#save#connect#clicked ~callback:(save ui));
-  ignore(ui#open_btn#connect#clicked ~callback:(open_dep ui));
-  ignore(ui#reload#connect#clicked ~callback:(reload ui));
-  ignore(ui#to_svg#connect#clicked ~callback:(to_svg ui));
-  ignore(ui#to_png#connect#clicked ~callback:(to_png ui));
-  ignore(ui#to_pdf#connect#clicked ~callback:(to_pdf ui));
+(* -------------------------------------------------------------------------------- *)
+let open_editor parent () =
+  let editor = new editor () in
+  editor#source#buffer#set_text !current_source;
+
+  ignore(editor#toplevel#connect#destroy ~callback:editor#toplevel#destroy);
+  ignore(editor#close_button#connect#clicked ~callback:editor#toplevel#destroy);
+
+  let refresh_current_source () =
+    current_source :=
+      editor#source#buffer#get_text 
+      ~start:editor#source#buffer#start_iter 
+      ~stop:editor#source#buffer#end_iter 
+      ~slice:true 
+      ~visible:true () in
+
+  ignore(editor#source#buffer#connect#changed
+           ~callback:(fun () -> refresh_current_source (); refresh_svg parent ()));
+
+
+  editor#check_widgets ();
+  editor#toplevel#show ();
+  ()
+
+(* -------------------------------------------------------------------------------- *)
+let main ?infos ?file () =
+  let _ = GMain.Main.init () in
+  let ui = new ui () in
+  let () = ui#toplevel#set_title ("Dep2pict v. "^VERSION) in
+  let _ = ui#toplevel#connect#destroy ~callback:GMain.Main.quit in
+  let _ = ui#edit#connect#clicked ~callback:(open_editor ui) in
+  let _ = ui#save#connect#clicked ~callback:(save ui) in
+  let _ = ui#save_as#connect#clicked ~callback:(save_as ui) in
+  let _ = ui#open_btn#connect#clicked
+    ~callback:(fun () -> open_dep ui (); refresh_svg ui ()) in
+  let _ = ui#reload#connect#clicked
+    ~callback:(fun () -> reload ui (); refresh_svg ui ()) in
+
+  let _ = ui#zoom#connect#value_changed
+      ~callback:
+      (fun () ->
+        current_zoom := ui#zoom#adjustment#value;
+        refresh_svg ui ()
+      ) in
+
+  ignore(ui#to_svg#connect#clicked ~callback:(export "svg" ui));
+  ignore(ui#to_png#connect#clicked ~callback:(export "png" ui));
+  ignore(ui#to_pdf#connect#clicked ~callback:(export "pdf" ui));
+
+  (match infos with
+    | None -> ()
+    | Some cst -> current_infos := cst
+  );
 
   (match file with
     | None -> ()
-    | Some f -> ui#dep_source#buffer#set_text (readFile f)
+    | Some f -> current_file := Some f; reload ui ()
   );
 
   refresh_svg ui ();
 
-  ignore(ui#dep_source#buffer#connect#changed ~callback:(refresh_svg ui));
-	
   ui#check_widgets ();
   ui#toplevel#show ();
   GMain.Main.main ()
--- src/main.ml
+++ src/main.ml
@@ -1,108 +1,132 @@
 open Printf
 open Log
 open Dep2pict
+
 IFDEF BUILD_GUI THEN
 open Gui
-
-let gui ?file () = Gui.main ?file ()
+let gui ?infos ?file () = Gui.main ?infos ?file ()
 ELSE
-let input_file = ref None
-let output_file = ref None
-
-let gui ?file () = Log.critical "Gui not available"
+let gui ?infos ?file () = Log.critical "Gui not available"
 END
-
-let _ = 
-	Log.set_active_levels [`INFO];
-	Log.set_info_label "DEP2PICT";
-	Log.set_critical_label "DEP2PICT";
-	Log.set_write_to_log_file false;
-	Log.set_info_foreground Log.f_green; 
-	Log.set_critical_foreground Log.f_red;
-	Log.set_critical_background Log.b_default;
-	Log.set_show_time true
 
 let version = VERSION
 
+let _ = 
+  Log.set_active_levels [`INFO];
+  Log.set_info_label "DEP2PICT";
+  Log.set_critical_label "DEP2PICT";
+  Log.set_write_to_log_file false;
+  Log.set_info_foreground Log.f_green; 
+  Log.set_critical_foreground Log.f_red;
+  Log.set_critical_background Log.b_default;
+  Log.set_show_time true
+
 let usage = String.concat "\n" [
+  "================================================================================";
+  "=-=-=-=-= dep2pict a tool to draw dependency graphs =-=-=-=-=";
+  "";
   "Usage:";
-  "  * dep2pict <options> -dep <dep_file> -o <output_file>";
-  "  * dep2pict <options> -conll <conll_file> -o <output_file>";
+  "  * dep2pict <options> input_file output_file    convert input_file into output_file" ;
+  "  * dep2pict <options> input_file                run the GUI with the given file";
+  "  * dep2pict <options>                           run the GUI with an empty graph";
+  "";
+  "Formats are guessed from file extension:";
+  "  * input formats are: dep, conll, xml";
+  "  * output formats are: png, svg, pdf, dep";
+  "";
   "Options:";
-  "  -png: set output format to png (this is the default)";
-  "  -pdf: set output format to pdf";
-  "  -svg: set output format to svg";
-  "  -dep: set output format to dep";
-  "  -features <string>: set features to display with CONLL input, string should contain a subset of {l,p,s,t,g,m,n} or A (for all). Default is \"\"";
-  "  -ref: add dotted links for ellpsis";
-  "  -v: display version number ("^version^")";
+  "  -i | --infos     <string> select infos to display: a '|' separated list of atoms from: \"lemma\", \"pos\", \"lpos\", \"all\", a feature name (default=\"pos\")";
+  "  -p | --position  <int> number of the dep structure to display when input file contains sequence (default=0)";
+  "  -v | --version   display version number ("^version^")";
+  "  -h | --help      show this help";
+  "  -t | --tips      add on_mouse_over tips in svg output (for debug purpose)";
+  "================================================================================";
 ]
 
-type output = Png | Svg | Pdf | Dep_o
-let output = ref Png
+let input_file = ref None
+let output_file = ref None
 
-type input = Dep | Conll | Xml of int 
-let input = ref Dep
+let position = ref 0
+let tips = ref false
 
-let debug = ref false
+let infos = ref []
 
-let eps_ref = ref false
-let conll_features = ref ""
+let rec parse_arg = function 
+  | [] -> ()
+  | "-v"::_ | "--version"::_ -> printf "%s\n%!" version; exit 0
+  | "-h"::_ | "--help"::_ -> printf "%s\n%!" usage; exit 0
+  | "-i"::i::tail 
+  | "--infos"::i::tail -> infos := Str.split (Str.regexp " *| *") i
 
-let _ =
-  let rec opt = function 
-    | [] -> ()
-    | "-v"::_ -> printf "%s\n%!" version; exit 0
-    | "-help"::_ -> printf "%s\n%!" usage; exit 0
+  | "-p"::i::tail 
+  | "--position"::i::tail -> position := (int_of_string i); parse_arg tail
 
-    | "-conll"::file::tail -> input_file := Some file; input := Conll; opt tail
-    | "-dep"::file::tail -> input_file := Some file; opt tail
+  | "-t"::tail | "--tips"::tail -> tips := true; parse_arg tail
 
-    (* NB: the xml option is used by parconine: arg is an integer, the xml_file is given with the -dep option *)
-    | "-xml"::n::tail -> input := Xml (int_of_string n); opt tail
-   
-    | "-o"::file::tail -> output_file := Some file; opt tail
-    | "-png"::tail -> output := Png; opt tail
-    | "-svg"::tail -> output := Svg; opt tail
-    | "-pdf"::tail -> output := Pdf; opt tail
-    | "-out_dep"::tail -> output := Dep_o; opt tail
+  | s::_ when s.[0] = '-' -> Log.fcritical "Unknwon option \"%s\"" s
 
-    | "-features"::feats::tail -> conll_features := feats; opt tail
-    | "-ref"::tail -> eps_ref := true; opt tail
+  | anon :: tail -> 
+    begin
+      match !input_file with
+        | None -> input_file := Some anon
+        | Some _ ->
+          match !output_file with
+            | None -> output_file := Some anon
+            | Some _ -> Log.fcritical "At most two anonymous arguments are allowed, don't know what to do with \"%s\"" anon
+    end;
+    parse_arg tail
 
-    | "-d"::tail -> debug := true; opt tail
+type format =  Dep | Conll | Xml | Png | Svg | Pdf
+exception Found of format
+let get_format file =
+  try 
+    List.iter
+      (fun (ext, fmt) ->
+        if Filename.check_suffix file ext
+        then raise (Found fmt)
+      ) [ ("dep", Dep); ("conll",Conll); ("xml",Xml); ("png",Png); ("svg",Svg); ("pdf",Pdf) ];
+    Log.fcritical "Unkwnow file extension for file \"%s\"" file
+  with Found fmt -> fmt
 
-    | others::tail -> Log.fcritical "%s : option unknown!\n%s" others usage
-  in opt (List.tl (Array.to_list Sys.argv));
+
+let _ = 
+  let () = parse_arg (List.tl (Array.to_list Sys.argv)) in 
 
   match (!input_file, !output_file) with
-    | (None, None) -> gui ()
-    | (None, Some _) -> Log.critical "No input file."
-    | (Some file,_) when not (Sys.file_exists file) -> Log.fcritical "The file %s doesn't exist." file
-    | (Some file, None) -> gui ~file ()
+    | (None, None) -> gui ~infos:(!infos) ()
+    | (Some file,_) when not (Sys.file_exists file) -> Log.fcritical "The input file %s doesn't exist." file
+    | (Some file, None) -> gui ~infos:(!infos) ~file ()
     | (Some in_file, Some out_file) ->
-      try
-        (match (!input, !output) with
-          | (Xml i, Png) -> ignore (Dep2pict.fromXmlFileToPng in_file out_file i)
-          | (Dep, Png) -> ignore (Dep2pict.fromDepFileToPng in_file out_file)
-          | (Conll, Png) -> ignore (Dep2pict.fromConllFileToPng ~features:!conll_features ~eps_ref:!eps_ref in_file out_file)
+      begin
+        try
+          match (get_format in_file, get_format out_file) with
+            | (Xml, Png) -> ignore (Dep2pict.fromXmlFileToPng in_file out_file !position)
+            | (Dep, Png) -> ignore (Dep2pict.fromDepFileToPng in_file out_file)
+            | (Conll, Png) -> ignore (Dep2pict.fromConllFileToPng ~infos:!infos in_file out_file)
 
-          | (Xml i, Svg) -> ignore (Dep2pict.fromXmlFileToSvgFile ~debug:(!debug) in_file out_file i)
-          | (Dep, Svg) -> ignore (Dep2pict.fromDepFileToSvgFile ~debug:(!debug) in_file out_file)
-          | (Conll, Svg) -> ignore (Dep2pict.fromConllFileToSvgFile ~debug:(!debug) ~features:!conll_features ~eps_ref:!eps_ref in_file out_file)
+            | (Xml, Svg) -> ignore (Dep2pict.fromXmlFileToSvgFile ~debug:(!tips) in_file out_file !position)
+            | (Dep, Svg) -> ignore (Dep2pict.fromDepFileToSvgFile ~debug:(!tips) in_file out_file)
+            | (Conll, Svg) -> ignore (Dep2pict.fromConllFileToSvgFile ~debug:(!tips) ~infos:!infos in_file out_file)
 
-          | (Xml i, Pdf) -> ignore (Dep2pict.fromXmlFileToPdf in_file out_file i)
-          | (Dep, Pdf) -> ignore (Dep2pict.fromDepFileToPdf in_file out_file)
-          | (Conll, Pdf) -> ignore (Dep2pict.fromConllFileToPdf ~features:!conll_features ~eps_ref:!eps_ref in_file out_file)
+            | (Xml, Pdf) -> ignore (Dep2pict.fromXmlFileToPdf in_file out_file !position)
+            | (Dep, Pdf) -> ignore (Dep2pict.fromDepFileToPdf in_file out_file)
+            | (Conll, Pdf) -> ignore (Dep2pict.fromConllFileToPdf ~infos:!infos in_file out_file)
           
-          | (Conll, Dep_o) -> Dep2pict.fromConllFileToDep ~features:!conll_features ~eps_ref:!eps_ref in_file out_file
-          | (_,  Dep_o) -> Log.fcritical "Conversion from Xml or Dep into Dep is not implemented. Please contact developers if your really need it!"
-        );
+            | (Conll, Dep) -> Dep2pict.fromConllFileToDep ~infos:!infos in_file out_file
 
+            | (Dep, Dep) 
+            | (Xml, Dep) -> Log.fcritical "Conversion from Xml or Dep into Dep is not implemented. Please contact developers if your really need it!"
+            | (_, Xml) -> Log.fcritical "Conversion to Xml is not implemented. Please contact developers if your really need it!"
+            | (Pdf, _) -> Log.fcritical "pdf in not a valid input format"
+            | (Svg, _) -> Log.fcritical "svg in not a valid input format"
+            | (Png, _) -> Log.fcritical "png in not a valid input format"
+            | (_, Conll) -> Log.fcritical "conll in not a valid output format"
+        with
+	  | Dep2pict.Parse_error msgs -> List.iter (fun (l,m) -> printf "Line %d: %s\n" l m) msgs; Log.fcritical "Parse error"
+	  | Dep2pict.Id_already_in_use_ id -> Log.fcritical "Id already in use : %s" id
+	  | Dep2pict.Unknown_index id -> Log.fcritical "Can't find index: %s" id
+	  | Dep2pict.Loop_in_dep msg -> Log.fcritical "Loop in dependency : %s" msg
+      end;
+      Log.finfo "File %s generated." out_file
+    | (None, Some _) -> Log.bug "Some output without input!"
 
-        Log.finfo "File %s generated." out_file
-      with
-	| Dep2pict.Parse_error msgs -> List.iter (fun (l,m) -> printf "Line %d: %s\n" l m) msgs; Log.fcritical "Parse error"
-	| Dep2pict.Id_already_in_use_ id -> Log.fcritical "Id already in use : %s" id
-	| Dep2pict.Unknown_index id -> Log.fcritical "Can't find index: %s" id
-	| Dep2pict.Loop_in_dep msg -> Log.fcritical "Loop in dependency : %s" msg
